47d9ce33722987f09b5ff8f4fdfd3b8ff4b69a80
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-10-31 13:12:37 +0530

    Package Created and All files included

---
+++ fifo_package.sv
@@ -0,0 +1,7 @@
+package fifo_package;
+    `include "fifo_transaction.sv"
+    `include "fifo_generator.sv"
+    `include "fifo_driver.sv"
+    `include "fifo_enviroment.sv"
+    `include "fifo_test.sv"
+endpackage
