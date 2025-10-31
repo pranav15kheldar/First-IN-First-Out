42db327a8781f8ef5a11f7318c17306f96aeac30
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-10-31 15:13:15 +0530

    Clean Compile

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -1,10 +1,10 @@
-interface fifo_interface();
-    logic clk ,
-    logic rst ,
-    logic wr_en ,
-    logic rd_en ,
-    logic [WIDTH-1 : 0] data_in ,
-    logic reg [WIDTH-1 : 0] data_out ,
-    logic reg full,
-    logic reg empt
+interface fifo_interface #(parameter WIDTH = 32 ) ;
+    logic clk;
+    logic rst;
+    logic wr_en;
+    logic rd_en;
+    logic [WIDTH-1 : 0] data_in;
+    logic [WIDTH-1 : 0] data_out;
+    logic full;
+    logic empt;
 endinterface
