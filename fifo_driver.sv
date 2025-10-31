6d0ee7091b821ce83e8ded8f15f800a6d78872e1
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-10-31 15:12:18 +0530

    Solve the error After Compile

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -3,14 +3,14 @@ class fifo_driver;
     fifo_transaction fifoTras_of_driver;
     mailbox gen2driver;
 
-    function new(mailbox gen2driver virtual fifo_interface vir_fifoInterface);
+    function new(mailbox gen2driver , virtual fifo_interface vir_fifoInterface);
         this.gen2driver = gen2driver;
         this.vir_fifoInterface = vir_fifoInterface;
     endfunction
     
     task main();
         forever begin
-            @ (posedge vir_fifoInterface.clk)
+            @ (posedge vir_fifoInterface.clk);
             vir_fifoInterface.data_in <= fifoTras_of_driver.data_in;
             fifoTras_of_driver.data_out <= vir_fifoInterface.data_out;
             $display("*************************** Values at Driver *************************** ");
