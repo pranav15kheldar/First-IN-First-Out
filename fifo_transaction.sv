ef32605255db6045a34060e54c6c3a98488d5ebc
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-11-03 15:04:43 +0530

    randomize read enable

--- fifo_transaction.sv
+++ fifo_transaction.sv
@@ -1,8 +1,8 @@
 class fifo_transaction #(parameter WIDTH = 32);
     logic clk;
     logic rst;
-    logic wr_en;
-    logic rd_en;
+    rand logic wr_en;
+    rand logic rd_en;
     rand logic [WIDTH-1 : 0] data_in;
     logic [WIDTH-1 : 0] data_out;
     logic full;
