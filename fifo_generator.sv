53fdbd3e6275f80313bf7daefdac92c2f75508fd
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-11-03 15:03:48 +0530

    Write constrint

--- fifo_generator.sv
+++ fifo_generator.sv
@@ -5,12 +5,16 @@ class fifo_generator;
         this.gen2driver = gen2driver;
     endfunction
 
+    constraint rd_en_c
+    {
+        fifoTras.wr_en ^ fifoTras.rd_en == 1;
+    }
     task main();
         repeat(10) begin
             fifoTras = new();  // Object Created For Transcation 
             fifoTras.randomize(); // Randomize 
             $display(" ***************** Data Generated In Generator *******************");
-            $display("Data in : %d" , fifoTras.data_in);
+            $display("Data in : %0d , Write Enable : %0d Reset : %0d" , fifoTras.data_in , fifoTras.wr_en , fifoTras.rst);
             gen2driver.put(fifoTras); // Put the data in mailbox to send to driver
         end
     endtask
