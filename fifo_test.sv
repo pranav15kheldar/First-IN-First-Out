91f45272c59b19e3827b74fc8d2392aa63c72fae
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-11-03 10:11:36 +0530

    Clean Compile

--- fifo_test.sv
+++ fifo_test.sv
@@ -3,12 +3,12 @@ class fifo_test;
     fifo_enviroment env_Of_test;
     function  new(virtual fifo_interface vir_inf_test);
         this.vir_inf_test = vir_inf_test;
-        env = new(vir_inf_test);
+        env_Of_test = new(vir_inf_test);
     endfunction
 
     task run();
         fork
-            env_Of_test.main();
+            env_Of_test.run();
         join
     endtask
 endclass
