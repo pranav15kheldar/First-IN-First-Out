0b68bb88150abe8a04bba56bdb19d11b067b2db7
Pranav Kheldar <kheldarpranav03@gmail.com>
2025-11-03 11:43:39 +0530

    Added Parameter and clean compile

--- fifo_transaction.sv
+++ fifo_transaction.sv
@@ -1,8 +1,10 @@
 class fifo_transaction #(parameter WIDTH = 32);
+    logic clk;
+    logic rst;
     logic wr_en;
     logic rd_en;
     rand logic [WIDTH-1 : 0] data_in;
     logic [WIDTH-1 : 0] data_out;
     logic full;
-    logic empt;
+    logic empty;
 endclass
